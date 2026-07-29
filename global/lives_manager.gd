c318c1ca432fe6ad3ae795ae82691e98de6e07b6
Luciano-24 <cb.luciano.vargas.r@upds.edu.bo>
2026-07-29 09:26:47 -0400

    agrega sistema global de vidas (LivesManager)

---
+++ global/lives_manager.gd
@@ -0,0 +1,25 @@
+extends Node
+
+signal lives_changed(lives: int)
+signal game_over
+
+const INITIAL_LIVES: int = 3
+
+var lives: int = INITIAL_LIVES
+
+func add_life(amount: int = 1) -> void:
+	lives += amount
+	lives_changed.emit(lives)
+
+func remove_life(amount: int = 1) -> void:
+	lives -= amount
+	lives_changed.emit(lives)
+	if lives <= 0:
+		game_over.emit()
+
+func get_lives() -> int:
+	return lives
+
+func reset_lives() -> void:
+	lives = INITIAL_LIVES
+	lives_changed.emit(lives)
